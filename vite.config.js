e4cbbc056352530dd81262acb9ebef97eda90e06
philipe <philipedarosa10@gmail.com>
2025-12-18 23:33:33 -0300

    deploy feito e problemas no responsivo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/to-do-list/', 
+  base: '/todo-list/', 
    server: {
     host: true
   }
