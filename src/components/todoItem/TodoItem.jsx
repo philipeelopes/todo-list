1877b499f7bc0b1956e0e35144537a45ba1cb232
philipe <philipedarosa10@gmail.com>
2025-12-22 13:22:04 -0300

    teste39

--- src/components/todoItem/TodoItem.jsx
+++ src/components/todoItem/TodoItem.jsx
@@ -2,32 +2,39 @@ import Styles from "./TodoItem.module.css";
 
 export default function TodoItem({ todo, onToggle, onDelete }) {
     return (
-        <li className={Styles.todoItem}>
-     
 
-            <label className={Styles.label}>
-                <input className={Styles.checkbox} type="checkbox"
+        <li className={Styles.todoItem}>
+
+            <div className={Styles.content}>
+                <input
+                    className={Styles.checkbox}
+                    type="checkbox"
                     checked={todo.completa}
                     onChange={() => onToggle(todo.id)}
+                    id={`todo-${todo.id}`}
                 />
-                <span className={todo.completa ? Styles.feito : ""}>{todo.texto}
-                               
-                </span>
-            </label>
 
-            <button className={Styles.btnDelete} onClick={() => {
-                if (confirm("Tem certeza que deseja excluir")) {
-                    onDelete(todo.id)
-                }
-            }}
+                <label
+                    htmlFor={`todo-${todo.id}`}
+                    className={`${Styles.text} ${todo.completa ? Styles.feito : ""}`}
+                >
+                    {todo.texto}
+                </label>
+            </div>
+
+            <button
+                className={Styles.btnDelete}
+                onClick={() => {
+                    if (confirm("Tem certeza que deseja excluir")) {
+                        onDelete(todo.id);
+                    }
+                }}
             >
-                 
-
                 Excluir
             </button>
 
-
         </li>
+
     )
 
 }
